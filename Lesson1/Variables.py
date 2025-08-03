256cab8e729471e31515d0f09e661fbcb3078951
SharanyaMenon22 <Sharanyamenon22@gmail.com>
2025-08-03 08:37:37 +0800

    My First Commit

---
+++ Lesson1/Variables.py
@@ -0,0 +1,45 @@
+# print("Hello, world!" )
+
+# variables.
+# the names given to the data. The data is stored / represented using a variable.
+
+
+marks = 90
+print("The marks are:", marks)
+
+marks = marks + 10
+print("After incrementing the marks by 10, we got: ", marks)
+
+# multiplication, substraction, division...
+
+marks = marks / 10
+
+print("After dividing marks by 10: ", marks)
+
+# numbers, text , characters ( a, b, c,...), True / False.
+
+marks = True
+
+print(marks)
+
+marks = marks + 10
+
+print(marks)
+
+marks = False
+
+print(marks)
+
+marks = marks + 10
+
+print(marks)
+
+# declare 2 variables with data and variables names of your choice.
+# do some operation with it and print the output.
+Marks = 95
+Increment = 10
+print("Marks", Marks)
+print("Incremented by",Increment)
+IncrementedMarks = Marks + Increment
+print("Marks incremented:", IncrementedMarks)
+print("Marks", Marks)
